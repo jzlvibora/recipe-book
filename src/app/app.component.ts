ce221fd7186d69acfc6e16c1ab6e2a583e9ab640
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-07-26 23:49:15 +0800

    added navigation using @output and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipe-angular';
+title = 'recipe-angular';
+activeLink='recipe';
+//  recipeLink=true;
+//  shopLink=false;
+
+  onLinkClicked(link:string){
+    this.activeLink=link;
+    // if(link='recipe'){
+    //   this.recipeLink=true;
+    //   this.shopLink=false;
+    // }
+    // if(link='shop'){
+    //   this.recipeLink=!this.recipeLink;
+    //   this.shopLink=!this.shopLink;
+    // }
+  }
 }
+
+
