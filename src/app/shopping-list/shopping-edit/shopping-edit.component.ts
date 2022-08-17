0905453f4840382a8ea15851e087984c4f526196
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-08-17 22:40:29 +0800

    added delete functionality

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -53,7 +53,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onClear(){
     this.slForm.reset();
     this.editMode=false;
+  }
 
+  onDelete(){
+    this.shoppingListService.deleteIngredient(this.editedItemIndex)
+    this.onClear();
   }
 
   ngOnDestroy() {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,29 +6,28 @@ import { ShoppingListService } from './shopping-list.service';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit , OnDestroy{
-  ingredients!:Ingredient[];
-  private subscription!:Subscription;
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  ingredients!: Ingredient[];
+  private subscription!: Subscription;
 
-  constructor(private shoppingListService:ShoppingListService) { }
-  
-  ngOnInit(){
-    this.ingredients=this.shoppingListService.getIngredients();
-    this.subscription=this.shoppingListService.ingredientsChanged.subscribe((ingredients:Ingredient[])=>{
-      this.ingredients=ingredients;
-    })
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.subscription = this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
 
-  onEditItem(index:number){
-this.shoppingListService.startedEditing.next(index);
+  onEditItem(index: number) {
+    this.shoppingListService.startedEditing.next(index);
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
-   }
- 
-
-
+  }
 }
