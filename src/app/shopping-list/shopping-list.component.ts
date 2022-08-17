8203bf1bdd99b224cc3d5d40888c60410c66bb1d
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-08-17 19:16:09 +0800

    replaced eventemitters w/o @output with Subject

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,14 +8,18 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
-  ingredients!:Ingredient[]
+export class ShoppingListComponent implements OnInit , OnDestroy{
+  ingredients!:Ingredient[];
+  private igChangeSub!:Subscription;
 
   constructor(private shoppingListService:ShoppingListService) { }
+  ngOnDestroy(): void {
+   this.igChangeSub.unsubscribe();
+  }
 
   ngOnInit(){
     this.ingredients=this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe((ingredients:Ingredient[])=>{
+    this.igChangeSub=this.shoppingListService.ingredientsChanged.subscribe((ingredients:Ingredient[])=>{
       this.ingredients=ingredients;
     })
   }
