3faa32bef708a8227c2ceca2a4ac4d926804e3f3
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-08-17 17:53:35 +0800

    completed new and edit recipe routing

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -11,7 +11,9 @@ import { RecipeService } from '../recipe.service';
 export class RecipeDetailComponent implements OnInit {
   recipe!: Recipe;
   id!:number;
-  constructor(private recipeService:RecipeService,private route:ActivatedRoute) { }
+  constructor(private recipeService:RecipeService,
+              private route:ActivatedRoute,
+              private router:Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params:Params)=>{
@@ -22,6 +24,11 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'], {relativeTo:this.route})
+    // this.router.navigate(['../', this.id, 'edit'],{relativeTo:this.route})
   }
 
 }
