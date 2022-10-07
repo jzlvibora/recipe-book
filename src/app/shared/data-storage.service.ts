edbf7d9fd8d91a5166b8480bd7b92322af896d41
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2022-10-07 20:13:05 +0800

    added resolver

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -19,8 +20,21 @@ export class DataStorageService {
   }
 
   fetchRecipes(){
-    this.http.get<Recipe[]>('https://ng-recipe-book-35a0f-default-rtdb.firebaseio.com/recipes.json').subscribe((res)=>{
-      this.recipeService.setRecipes(res)
+    return this.http.get<Recipe[]>('https://ng-recipe-book-35a0f-default-rtdb.firebaseio.com/recipes.json')
+    .pipe(map(recipes=>{
+      return recipes.map(recipe=>{
+        return{
+          ...recipe,
+          ingredients:recipe.ingredients?recipe.ingredients:[]
+        };
+      });
+    }),
+    tap(recipes=>{
+      this.recipeService.setRecipes(recipes);
     })
+    )
+    // .subscribe((res)=>{
+    //   this.recipeService.setRecipes(res)
+    // })
   }
 }
